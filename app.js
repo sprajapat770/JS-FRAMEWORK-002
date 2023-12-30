83eef29b35eea5a721b34186679ea44333ee70cf
Suraj Prajapat <sprajapat770@gmail.com>
2023-12-30 12:47:34 +0530

    express server

--- app.js
+++ app.js
@@ -14,13 +14,31 @@ app.get('/', (req, res) => {
     
     //express response object
     // res.send('<p>home page</p>');
-    
+
+    //send file response
+    res.sendFile('./view/index.html', {root: __dirname });
 });
 
 app.get('/about', (req, res) => {
     //express response object
     // res.send('<p>About Page</p>');
+
+    //send file response
+    res.sendFile('./view/about.html', {root: __dirname });
 });
 
+
+//redirects
+app.get('/about-us', (req, res) => {
+    //send file response
+    res.redirect('/about');
+});
+
+//404 page
+app.use((req, res) => {
+    //use function trigger for every single request //this should be at bottom
+    //send file response
+    res.sendFile('./view/404.html', {root: __dirname });
+});
 
 //some concept of websocket connection possible with express
